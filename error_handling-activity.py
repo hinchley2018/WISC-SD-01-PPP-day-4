7b8c477cd2b85717bb61761eacf359f01ff3f38a
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2022-03-28 21:20:12 -0500

    venv and show golf game

--- error_handling-activity.py
+++ error_handling-activity.py
@@ -1,13 +1,60 @@
-fileName = input("Please enter the name of the file you would like to read: ")
-myfile = open(fileName, 'r') # Open a file for reading.
-contents = myfile.readlines() # Read in the content by line.
-myfile.close() # Explicitly close the file
-print(contents) #print the contents of the file
+# fileName = input("Please enter the name of the file you would like to read: ")
+# myfile = open(fileName, 'r') # Open a file for reading.
+# contents = myfile.readlines() # Read in the content by line.
+# myfile.close() # Explicitly close the file
+# print(contents) #print the contents of the file
 
+class FileManipulator:
+    def __init__(self, fileName):
+        self.contents = None
+        #try until we have contents
+        while self.contents == None:
+            try:
+                myfile = open(fileName, 'r')
+                self.contents = myfile.readlines()
+            #handle various file exceptions
+            except (FileNotFoundError, TypeError, OSError) as e:
+                #debg
+                print(type(e), e)
+                # prompt again
+                fileName = input("Please enter a valid file name")
+    def reverse(self, fileName):
+        
+        #open file for writing
+        try: 
+            myfile = open(fileName, 'w')
+            #:-1 give me all contents
+            #::-1 reverse the contents
+            reversed_contents = [x.strip()[::-1] for x in self.contents]
+            print("reversed",reversed_contents)
+            for i in range(len(reversed_contents)):
+                #try writing the file
+                # the last line
+                if (i == len(reversed_contents) - 1):
+                    myfile.write(reversed_contents[i])
+                else:
+                    myfile.write(reversed_contents[i] + '\n')
+        except (FileNotFoundError, TypeError, OSError) as e:
+            #debg
+            print(type(e), e)
+            # prompt again
+            fileName = input("Please enter a valid file name")            
+        finally:
+            myfile.close()
 
-
-
+    def upper(self, fileName):
+        try:
+            myfile = open(fileName, 'w')
+            upper_contents = [x.strip().upper() for x in self.contents]
+            for i in range(len(upper_contents)):
+                myfile.write(upper_contents[i]+'\n')
+        except (FileNotFoundError, TypeError, OSError) as e:
+            print(type(e),e)
+            fileName = input("Please enter a valid file name: ")
+        finally:
+            myfile.close()
 # test it on a file that doesn't exist should throw an error
 f = FileManipulator("tmp.txt")
 print(f.contents)
-f.reverse("tmp2.txt")
+f.reverse("tmp-reversed.txt")
+f.upper("tmp-upper.txt")
--- golf-game.py
+++ golf-game.py
@@ -1,3 +1,6 @@
+import requests
+import pprint
+from datetime import datetime
 # the activity is broken use this as a mock response 
 weatherMockResponse = {
     "description": "Partly cloudy",
@@ -9,3 +12,30 @@ weatherMockResponse = {
     "temperature": "21 C",
     "wind": "9 km/h"
 }
+#make a request to weather Api
+#API is broken returns 404 not found...
+#API_URL = "https://goweather.herokuapp.com/weather/" 
+# I wrote this node api that gives the mock response above 
+API_URL = "https://weather-api-node-wisc.herokuapp.com/weather/seattle"
+city = "seattle"
+r = requests.get(API_URL + city)
+response = r.json()
+print(response)
+
+forecast_list = response['forecast']
+today = datetime.now().strftime("%b-%d-%Y")
+
+# make data in format for graphing library
+to_graph = {}
+#trakc day past current dt
+count = 1
+
+for day in forecast_list:
+    current_date = int(today[4:6]) + count
+    #the activity incorrectly shows you this_day = f"{today[0:4]}{current_date}{this_day[6:]}"
+    #correct line is below since this_day is being defined so you can't refrenece itself
+    this_day = f"{today[0:4]}{current_date}{today[6:]}"
+    count+=1 if current_date <= 31 else 1
+    to_graph[this_day] = day['wind']
+
+print(to_graph)
